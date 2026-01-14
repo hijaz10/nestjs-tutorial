5a5c982d2103483e72ef7282ca8d807a81e8042d
HIJAZ <muhdhijaz10@gmail.com>
2026-01-14 18:33:52 +0100

    initial commit

---
+++ nest/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [UserModule, BookmarkModule, AuthModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
